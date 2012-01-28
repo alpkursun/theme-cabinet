4ad8ff80073ebfa1eedfaa07db5f7fee005bc79e
Alp Kursun <al.kursun@gmail.com>
2012-01-28 11:51:33 +1100

    Updated to start file listener when server initializes

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,3 +1,6 @@
 # This will initialize the configuration in ../config.yml
 
 APP_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/config.yml")[Rails.env]
+
+# Start the file listener which will listen for FTP files
+FileListener.instance.start_listening
