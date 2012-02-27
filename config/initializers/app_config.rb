3068045f280604e7c6616b1da4ed1747f4badc78
Alp Kursun <al.kursun@gmail.com>
2012-02-28 00:08:37 +1100

    fixed automatic directory creation code

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -2,10 +2,8 @@
 
 APP_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/config.yml")[Rails.env]
 
-directories_reqd = APP_CONFIG.map{|k, v| /.*path/.match(k); v}
-
 #make the directories if they dont exist
-FileUtils.mkdir_p(directories_reqd)
+APP_CONFIG.map{|k, v| if /.*path/.match(k); FileUtils.mkdir_p(v); end}
 
 # Start the file listener which will listen for FTP files
 FileListener.instance.start_listening
