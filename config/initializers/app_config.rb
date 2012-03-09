501bac4d5dd42bfed28ef1b9e5e80549545cba7f
Alp Kursun <al.kursun@gmail.com>
2012-03-09 18:41:18 +1100

    fixed location of FileListener shut down hook to avoid rake asset:precompile errors

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -7,3 +7,6 @@ APP_CONFIG.map{|k, v| if /.*path/.match(k); FileUtils.mkdir_p(v); end}
 
 # Start the file listener which will listen for FTP files
 FileListener.instance.start_listening
+
+# Register exit handler call to stop the file listener thread
+at_exit { FileListener.instance.stop_listening }
